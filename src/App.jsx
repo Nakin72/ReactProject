ae2599dccc5ffe28fae7b2a13c281fce042e0441
Nikita <RepackRussian@gmail.com>
2024-04-22 18:30:47 +0500

    Added images and made interesting design for skill

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,22 @@
-import { useState } from 'react'
-import avatarImg from '/public/AvatarLogo.jpg'
 
+//import avatarImg from '/public/Avatars/AvatarLogo.jpg'
+import SkillTag from "./Components/JSX/SkillTag"
 import './App.css'
-import * as React from 'react';
-import Skeleton from '@mui/material/Skeleton';
-import Stack from '@mui/material/Stack';
+import Logos from "./Components/JS/LogoSelector"
+
+
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="Profile">
-        <div className="ImgAndTextCard">
-          <img
-            className="Avatar"
-            src={avatarImg}
-          />
-
-          <p className="FullName">Наумов Никита</p>
-
-        </div>
-
-
-        <div className="SkillsBlock">
-          <p className="SkList">Список навыков</p>
-
-
-          <div className="Skill">
-            <p className="CPP">C++</p>
-            <p className="LevelBasic">Basic</p>
-          </div>
-
-          <div className="Skill">
-            <p className="CSharp">C#</p>
-            <p className="LevelBasic">Basic</p>
-          </div>
-
-          <div className="Skill">
-            <p className="Python">Python</p>
-            <p className="LevelBasic">Basic</p>
-          </div>
-
-          <div className="Skill">
-            <p className="Figma">Figma</p>
-            <p className="LevelBasic">Basic</p>
-          </div>
-        </div>
+      <div>
+        <SkillTag name={Logos[0]["name"]} img={Logos[0]["img"]} />
+        <SkillTag name={Logos[1]["name"]} img={Logos[1]["img"]} />
+        <SkillTag name={Logos[2]["name"]} img={Logos[2]["img"]} />
+        <SkillTag name={Logos[3]["name"]} img={Logos[3]["img"]} />
+        <SkillTag name={Logos[4]["name"]} img={Logos[4]["img"]} />
+        <SkillTag name={Logos[5]["name"]} img={Logos[5]["img"]} />
+        <SkillTag name={Logos[6]["name"]} img={Logos[6]["img"]} />
       </div>
 
     </>
@@ -54,26 +25,3 @@ function App() {
 
 export default App
 
-/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-     
-      <h1>Vite + React</h1>
-      <div className="card" >
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-
-      </p> */
---
+++ src/Components/JS/LogoSelector.js
@@ -0,0 +1,78 @@
+import CSharpLogo from '@/L_logos/pngjpg/CSharpLogo.png'
+import CPPLogo from "@/L_logos/pngjpg/C++Logo.png"
+import PythonLogo from "@/L_logos/pngjpg/PythonLogo.png"
+import ArduinoLogo from "@/L_logos/pngjpg/ArduinoLogo.png"
+import Fusion360Logo from "@/L_logos/pngjpg/Fusion360Logo.png"
+import FigmaLogo from "@/L_logos/pngjpg/FigmaLogo.png"
+
+import DebianLogo from "@/L_logos/pngjpg/DebianLogo.png"
+import DockerLogo from "@/L_logos/pngjpg/DockerLogo.png"
+import NodeJSLogo from "@/L_logos/pngjpg/NodeJSLogo.png"
+import PostgreSQLLogo from "@/L_logos/pngjpg/PostgreSQLLogo.png"
+import RaspberryPiLogo from "@/L_logos/pngjpg/RaspberryPiLogo.png"
+import ReactLogo from "@/L_logos/pngjpg/ReactLogo.png"
+
+const Logos = [
+    {
+        id: 1,
+        name: "C#",
+        img: CSharpLogo
+    },
+    {
+        id: 2,
+        name: "C++",
+        img: CPPLogo
+    },
+    {
+        id: 3,
+        name: "Arduino",
+        img: ArduinoLogo
+    },
+    {
+        id: 4,
+        name: "Python",
+        img: PythonLogo
+    },
+    {
+        id: 5,
+        name: "Figma",
+        img: FigmaLogo
+    },
+    {
+        id: 6,
+        name: "Fusion360",
+        img: Fusion360Logo
+    },
+    {
+        id: 7,
+        name: "Debian",
+        img: DebianLogo
+    },
+    {
+        id: 8,
+        name: "Docker",
+        img: DockerLogo
+    },
+    {
+        id: 9,
+        name: "NodeJS",
+        img: NodeJSLogo
+    },
+    {
+        id: 10,
+        name: "PostgreSQL",
+        img: PostgreSQLLogo
+    },
+    {
+        id: 11,
+        name: "React",
+        img: ReactLogo
+    },
+    {
+        id: 12,
+        name: "RaspberryPi",
+        img: RaspberryPiLogo
+    }
+];
+
+export default Logos;
